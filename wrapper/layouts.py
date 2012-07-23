afa59987191150c369f30745a1352240de2e26ee
Chrislain Jemba E <cjemba@eurodns.com>
2012-07-23 16:43:49 +0200

    Add comments to layouts functions in layouts.py

--- wrapper/layouts.py
+++ wrapper/layouts.py
@@ -2,6 +2,8 @@ from wrapper.styles import COVER_TITLE_1, COVER_TITLE_2
 from reportlab.platypus import Paragraph, Frame, Image, PageBreak, KeepTogether
 
 def firstCover(pdf):
+  ''' Settings to apply to the current (first) page '''
+
   pdf.rectangle(color='#336391',height='50%',top=0)
   pdf.rectangle(color='#e6eeee',height='50%',bottom=0)
   pdf.rectangle(color='#ffffff',width=260,height=322,left='center',top='center')
@@ -11,5 +13,7 @@ def firstCover(pdf):
 
 
 def lastCover(pdf):
+  ''' Settings to apply to the current (last) page '''
+
   pdf.padding(left=0,right=0,bottom=0,top=0)
   pdf.color('#e6eeee')
