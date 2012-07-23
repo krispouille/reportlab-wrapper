c4c4d543a7b7094e743e38a169ccc1c643892110
Chrislain Jemba E <cjemba@eurodns.com>
2012-07-23 15:03:54 +0200

    Clean test.py example
    Remove unused imports
    Add comments

--- test.py
+++ test.py
@@ -1,18 +1,25 @@
-from wrapper.pdf      import Document, A4, landscape, portrait
-from wrapper.styles   import p, h2, h3, table
+from wrapper.pdf      import Document, A4
+from wrapper.styles   import p, h2, h3
 from wrapper.layouts  import first_cover, last_cover
 
+# Initialize PDF
 pdf = Document('test.pdf', title='My Title Test', pagesize=A4)
 
+# start page applying a specific layout "first_cover"
 pdf.page(layout=first_cover)
 
+# change paddings for next pages by setting all to 40 points
 pdf.padding(left=40,right=40,top=40,bottom=40)
 
+# start page applying no layout (defaults settings)
 pdf.page(
   h2('About This Document'),
   p('This document explains my attempt to wrap the current Reportlab Toolkit API in order to make PDFs easily.'),
   h3('How it works')
 )
 
+# start page applying a layout "last_cover"
 pdf.page(layout=last_cover)
+
+# save and close document
 pdf.close()
