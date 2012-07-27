62e79b7069882f34cfaa64d7a38214f774fcc47b
Chrislain Jemba E <cjemba@eurodns.com>
2012-07-27 13:49:25 +0200

    Add example 2 - Configure a page

---
+++ 2_configure_a_page.py
@@ -0,0 +1,15 @@
+from wrapper.pdf import Document, landscape, A4
+
+output = 'pdf/'+__file__[:-3]+'.pdf'
+
+# open a A4 document in landscapte and  with no paddings
+pdf = Document(output, title='Configure a page', pagesize=landscape(A4))
+
+# create a new page :
+# set a background color
+# override paddings from 0 to 10
+# override pagesize from landscape to portrait
+pdf.page(color='#336391', left=10, right=10, top=10, bottom=10, pagesize=A4)
+
+# save and close
+pdf.close()
